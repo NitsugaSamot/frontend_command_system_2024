7740b147d5b90a383223dd5c296c7573d3990c68
Tomas <69489194+NitsugaSamot@users.noreply.github.com>
2024-01-10 22:22:52 -0300

    modified login

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -4,6 +4,7 @@ import Alert from "../../components/alert/Alert"
 import clienteAxios from "../../config/clienteAxios"
 import useAuth from "../../hooks/useAuth"
 import Logo from '../../../public/chef-logo.jpg'
+import axios from 'axios'
 
 
 const Login = () => {
@@ -15,6 +16,8 @@ const Login = () => {
     const {setAuth} = useAuth()
   
     const navigate = useNavigate()
+
+    const backendURL = 'https://backendcommandsystem2024-production.up.railway.app';
 
     const handleSubmit = async e => {
         e.preventDefault()
@@ -28,7 +31,7 @@ const Login = () => {
         }
     
         try {
-          const {data} = await clienteAxios.post('/auth/login', {name, password})
+            const { data } = await axios.post(`${backendURL}/api/auth/login`, { name, password });
           setAlert({})
           localStorage.setItem('token', data.token)
           setAuth(data)
